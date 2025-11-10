2782fc964a3658c95e508e6ac1f7a026f4383eff
comardom <Comardom@outlook.com>
2025-11-10 22:17:34 +0800

    0.1.0beta补丁1

--- splash/FirstSplash.cpp
+++ splash/FirstSplash.cpp
@@ -2,7 +2,6 @@
 // Created by comardom on 2025/11/8.
 //
 
-// You may need to build the project (run Qt uic code generator) to get "ui_FirstSplash.h" resolved
 
 #include "FirstSplash.h"
 #include "ui_FirstSplash.h"
